97ce6164a9976140aedb6dba1a0e077b3ceadf86
Benedikt <info@benediktgrether.de>
2024-11-22 08:49:20 +0100

    Update import

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import Login from '@/components/Login';
+import Login from '@appwrite_auth/components/Login';
 
 export default function Home() {
     return <Login />;
--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
-import { Input } from '@/components/ui/input';
-import { Button } from '@/components/ui/button';
+import { Input } from '@appwrite_auth/components/ui/input';
+import { Button } from '@appwrite_auth/components/ui/button';
 
 interface AuthFormProps {
     handleLogin: (email: string, password: string) => void;
